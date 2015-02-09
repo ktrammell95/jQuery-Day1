1b44f97b13a9d860760683c93c32de705057ba40
Katherine Trammell <ktrammell@gmail.com>
2015-02-09 14:08:54 -0500

    Pushing commit to test
    Receiving error message
    
    error: src refspec master does not match any.
    error: failed to push some refs to 'git@github.com:ktrammell95/jQuery-Day1.git'

---
+++ jQuery.js
@@ -0,0 +1,15 @@
+var dateCounter = function() {
+var date = new Date();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
+
+}
+
+$(function() {
+  dateCounter();
+  setInterval(counter, 1000);
+
+})
+
+$(function)
