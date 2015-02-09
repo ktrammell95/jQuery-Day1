df13e8583620d860eb78aa8364e96f1a3c3e6ba5
Katherine Trammell <ktrammell@gmail.com>
2015-02-09 18:31:35 -0500

    Completed Clock and Hex count
    Completed clock and added the 0 factor to the hour and minutes
    Completed the hex count and verified that the background color changes
    
    Added gitignore file
    Added Readme file

--- jQuery.js
+++ jQuery.js
@@ -1,15 +1,72 @@
-var dateCounter = function() {
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
+// var dateCounter = function() {
+// var date = new Date();
+// var hours = date.getHours();
+// var minutes = date.getMinutes();
+// var seconds = date.getSeconds();
 
-}
+var counter  = function() {
+
+  var date   = new Date();
+  var hour   = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds(); 
+
+  if (hour >= 13) {
+      hour = hour - 12;
+  }
+  if (hour.toString().length < 2) {
+      hour = "0" + hour;
+  }
+    if (minute.toString().length < 2) {
+      minute = "0" + minute;
+  }
+  if (second.toString().length < 2) {
+      second = "0" + second;
+  };
+  
+  var clock  = hour + ":" + minute + ":" + second;
+
+  $(".time").text(clock)
+  
+};
 
 $(function() {
-  dateCounter();
   setInterval(counter, 1000);
+});
 
-})
 
-$(function)
+
+var hexCounter  = function() {
+
+  var date   = new Date();
+  var hour   = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds(); 
+
+  if (hour.toString().length < 2) {
+      hour = "0" + hour;
+  }
+  if (minute.toString().length < 2) {
+      minute = "0" + minute;
+  }
+  if (second.toString().length < 2) {
+      second = "0" + second;
+  };
+
+  var hexClock  = "#" + hour + minute + second;
+  
+  $(".hex").text(hexClock)
+  .css("background-color", hexClock);
+  
+};
+
+$(function() {
+  setInterval(hexCounter, 1000);
+});
+
+
+
+
+
+
+
